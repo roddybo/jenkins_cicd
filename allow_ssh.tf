fce1a492a2ca549442ee7d32146cd220a38e1455
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-12 03:00:07 -0800

    Update allow_ssh.tf

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -1,26 +1,25 @@
-resource "aws_vpc" "sg-0514a63dffef6e5fe" {
-       name        = "secure ssh"
-       description = "allow ssh from my home ip"
-       vpc_id      = "${vpc-02ef769587dd081cf.id}"
-       tags {
-         Name = "secure_ssh"
-       }
+resource "aws_security_group" "allow_ssh" {
+  name        = "secure_ssh"
+  description = "Allow SSH from my home IP"
+  vpc_id      = aws_vpc.vpc-02ef769587dd081cf.id
 
-       #Not redundant - Inbound Access
-       ingress {
-         from_port   = "22"
-         to_port     = "22"
-         protocol    = "TCP"
-         cidr_blocks = ["73.116.216.200/32","76.102.161.122","98.45.139.47"]
-       }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["73.116.216.200"]  # Replace with your home IP
+  }
 
-       # Outbound Access    
-       egress {
-         from_port   = 0
-         to_port     = 0
-         protocol    = "-1"
-         cidr_blocks = ["0.0.0.0/0"]
-       }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_sg_tag"
+  }
 }
 
 
