763599c2ae0df2574e6a0681c7b0acdaa449d4e7
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-12 12:05:18 -0800

    Update allow_ssh.tf

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "my_sg" {
          from_port   = "22"
          to_port     = "22"
          protocol    = "TCP"
-         cidr_blocks = ["73.116.216.200/32"]
+         cidr_blocks = ["73.116.216.200/32","76.102.161.122","98.45.139.47"]
        }
 
        # Outbound Access    
