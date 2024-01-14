8078c2cbe9d3f4b997aa00173a72588788f99c44
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-14 15:59:55 -0800

    Update install_apache.tf

--- install_apache.tf
+++ install_apache.tf
@@ -1,19 +1,6 @@
-resource "aws_instance" "ubuntu_instance" {
-  ami           = "ami-0c7217cdde317cfec"  # Replace with your Ubuntu AMI ID
-  instance_type = "t2.micro"                # Replace with your instance type
-  key_name      = "aws_key_pair.autodeploy.key_name"      # Replace with your key pair name
-
-  subnet_id             = "subnet-0e7aa15edbd6d56ad"           # Replace with your subnet ID
-  vpc_security_group_ids = ["sg-0f4f7384e317671fc"] # Replace with your security group ID
-
-  tags = {
-    Name = "MyInstance"
-  }
-
-  provisioner "remote-exec" {
+provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y apache2",
-    ]
-  }
-}
+      "sudo systemctl enable apache2",
+      "sudo systemctl start apache2"
