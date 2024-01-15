cb9f8061fb8ad0a81f60dc0c2a9e8a99ae7a7fb5
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-14 16:11:04 -0800

    Update install_apache.tf

--- install_apache.tf
+++ install_apache.tf
@@ -1,27 +1,4 @@
-resource "aws_instance" "public_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.autodeploy.key_name
-
-  // Other instance configurations...
-
-  // Attach the primary EBS volume (default size)
-  root_block_device {
-    volume_size = 8  # Adjust the size based on your needs
-  }
-
-  // Attach an additional EBS volume (2 GB)
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    volume_size = 2
-    volume_type = "gp2"  # Adjust volume type as needed
-  }
-
-  tags = {
-    Name = var.name_tag
-  }
-
-  provisioner "remote-exec" {
+provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y apache2",
